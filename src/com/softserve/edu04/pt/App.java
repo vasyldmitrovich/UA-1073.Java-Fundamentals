696aaf9ee868cb6420a5f1444278db1eb39d467c
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2023-09-18 19:34:53 +0300

    Task 1_1 edu04

---
+++ src/com/softserve/edu04/pt/App.java
@@ -0,0 +1,9 @@
+package com.softserve.edu04.pt;
+
+public class App {
+    public static void main(String[] args) {
+
+        //Task 1_1
+        Task1.task1_1();
+    }
+}
---
+++ src/com/softserve/edu04/pt/Task1.java
@@ -0,0 +1,7 @@
+package com.softserve.edu04.pt;
+
+public class Task1 {
+    public static void task1_1 (){
+        System.out.println("Hello World");
+    }
+}
