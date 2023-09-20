3fadcdf95eaf11b2e8de7ef9b6e92cff881507e7
Andrii Berchak <berchak4@gmail.com>
2023-09-21 00:01:04 +0300

    My test commit

--- src/com/softserve/test/App.java
+++ src/com/softserve/test/App.java
@@ -1,4 +1,4 @@
-package com.softserve.test
+package com.softserve.test;
 
 public class App{
 
---
+++ src/com/softserve/test/ttt/TestTask.java
@@ -0,0 +1,7 @@
+package com.softserve.test.ttt;
+
+public class TestTask {
+    static void task(){
+        System.out.println("Hello from task!");
+    }
+}
