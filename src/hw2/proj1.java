233dfc34a41370ab1222ab1d101b1ea93ed0bf37
maxpv026 <maxym940@gmail.com>
2023-09-21 19:00:36 +0300

    2_hw

---
+++ src/hw2/proj1.java
@@ -0,0 +1,17 @@
+package hw2;
+
+import java.util.Scanner;
+
+class Main1 {
+    public class Main {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);//declaring user input
+
+            System.out.println("Input radius of flower bed: ");
+            int radius = scan.nextInt();
+
+            double res = 2 * Math.PI * radius;//calculating
+            System.out.println("The result is: " + res);
+        }
+    }
+}
