34dd0f3f65f5b3c37ac627c8fef1b4b00e175677
rodionov_artem <artemrodionovit@gmail.com>
2023-09-23 10:19:48 +0300

    Test commit

---
+++ src/com/softserve/edu04/hw/App.java
@@ -0,0 +1,5 @@
+package com.softserve.edu04.hw;
+
+public class App {
+// My comment
+}
---
+++ src/com/softserve/edu04/pt/Task1.java
@@ -0,0 +1,8 @@
+package com.softserve.edu04.pt;
+
+public class Task1 {
+    public static void task1_1 (){
+        System.out.println("Hello Artem!");
+
+    }
+}
