1f2cf540f9959f12c5a9de545c10b8b23dce3acd
Zamorii <sezamua@gmail.com>
2023-09-24 22:46:11 +0300

    Practical tasks 1 - 2, homework task 1 - 3 in edu03 done

---
+++ src/com/softserve/edu03/hw/Task1.java
@@ -0,0 +1,22 @@
+package com.softserve.edu03.hw;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ROOT);
+        Scanner console = new Scanner(System.in);
+        System.out.println("Input side 1:");
+        int a = console.nextInt();
+        System.out.println("Input side 2:");
+        int b = console.nextInt();
+        System.out.println("Input side 3:");
+        int c = console.nextInt();
+        System.out.println("The area of this triangle is " + triangleArea(a, b, c));
+    }
+
+    public static double triangleArea(int a, int b, int c) {
+        return Math.sqrt((a + b + c) * (b + c - a) * (a + c - b) * (a + b - c) / 16.0);
+    }
+}
---
+++ src/com/softserve/edu03/hw/Task2.java
@@ -0,0 +1,22 @@
+package com.softserve.edu03.hw;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ROOT);
+        Scanner console = new Scanner(System.in);
+        System.out.println("Input the first number:");
+        int a = console.nextInt();
+        System.out.println("Input the second number:");
+        int b = console.nextInt();
+        System.out.println("Input the third number:");
+        int c = console.nextInt();
+        System.out.println("The minimal value is " + minOfThreeValues(a, b, c));
+    }
+
+    public static int minOfThreeValues(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+}
