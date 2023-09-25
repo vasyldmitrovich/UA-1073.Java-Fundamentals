b2ded8757450cb46b0a86321abc08befec5b2d4f
arsentiev_artur <arturarsentiev25@gmail.com>
2023-09-25 09:55:44 +0300

    Task 1 PT

--- src/com/softserve/edu03/hw/App.java
+++ src/com/softserve/edu03/hw/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        // Homeworks 1992413
+
 
     }
 }
---
+++ src/com/softserve/edu03/hw/Task1.java
@@ -0,0 +1,27 @@
+package com.softserve.edu03.hw;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ROOT);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the length of the side of the triangle");
+        double sideTriniagle = scan.nextDouble();
+
+        System.out.println("Enter the height of the triangle");
+        double heightTriangle = scan.nextDouble();
+
+        double areaTriniagle = (sideTriniagle * heightTriangle) * 0.5;
+        System.out.println("Area of your triangle: " + areaTriniagle);
+
+    }
+
+
+
+}
+
+
+
