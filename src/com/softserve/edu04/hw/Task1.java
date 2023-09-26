4037e58ceb80db459c9cb5fb20b4dc6a9e97dda4
Zamorii <sezamua@gmail.com>
2023-09-27 01:41:34 +0300

    Practical tasks 1 - 5, homework 1 - 3 in edu04 done

---
+++ src/com/softserve/edu04/hw/Task1.java
@@ -0,0 +1,16 @@
+package com.softserve.edu04.hw;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void run() {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter 3 float numbers:");
+        float num1 = console.nextFloat();
+        float num2 = console.nextFloat();
+        float num3 = console.nextFloat();
+        boolean result = num1 >= -5 && num1 <= 5 && num2 >= -5 && num2 <= 5 && num3 >= -5 && num3 <= 5;
+        System.out.println(result ? "All numbers are in range from -5 to 5" : "Some numbers are not in range from -5 to 5");
+    }
+}
---
+++ src/com/softserve/edu04/hw/Task4.java
@@ -0,0 +1,7 @@
+package com.softserve.edu04.hw;
+
+public class Task4 {
+    public static void run() {
+
+    }
+}
