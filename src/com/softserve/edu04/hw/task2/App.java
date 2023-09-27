ad4955bca6b301ebd064c1f3be7303366fa190e8
Nazar <nazarvlk793@gmail.com>
2023-09-27 18:06:30 +0300

    fix task 2

--- src/com/softserve/edu04/hw/task2/App.java
+++ src/com/softserve/edu04/hw/task2/App.java
@@ -1,6 +1,6 @@
 package com.softserve.edu04.hw.task2;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class App {
@@ -19,27 +19,23 @@ public class App {
     }
 
     public static void outputMaxAndMin(int num1, int num2, int num3) {
-//        int max = 0;
-//        int min = 0;
-//        if (num1 > num2 && num1 > num3) {
-//            max = num1;
-//        } else if (num2 > num1 && num2 > num3) {
-//            max = num2;
-//        } else if (num3 > num1 && num3 > num2) {
-//            max = num3;
-//        }
-//        if (num1 < num2 && num1 < num3) {
-//            min = num1;
-//        } else if (num2 < num1 && num2 < num3) {
-//            min = num2;
-//        } else if (num3 < num1 && num3 < num2) {
-//            min = num3;
-//        }
-        int[] nums = new int[] {num1, num2, num3};
+        int max;
+        int min;
 
-        System.out.println("Max: " + Arrays.stream(nums).max().getAsInt());
-        System.out.println("Min: " + Arrays.stream(nums).min().getAsInt());
-//        System.out.println("Max: " + max);
-//        System.out.println("Min: " + min);
+        if (num1 > num2) {
+            max = num1;
+            min = num2;
+        } else {
+            max = num2;
+            min = num1;
+        }
+        if (max < num3) {
+            max = num3;
+        }
+        if (min > num3) {
+            min = num3;
+        }
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
     }
 }
