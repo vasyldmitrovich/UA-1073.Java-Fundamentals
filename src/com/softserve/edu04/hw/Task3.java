3ebf8ccf31a184078fe1bbec66e5fd64ccc60dd7
rodionov_artem <artemrodionovit@gmail.com>
2023-10-02 13:19:06 +0300

    My Task3

---
+++ src/com/softserve/edu04/hw/Task3.java
@@ -0,0 +1,30 @@
+package com.softserve.edu04.hw;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the number HTTPError: ");
+        int number = scanner.nextInt();
+        int name;
+        System.out.println("HTTPError name: ");
+
+    }
+}
+
+enum Name {
+    BadRequest, Unauthorized, PaymentRequired;
+}
+
+enum Number {
+    _400(Name.BadRequest),
+    _401(Name.Unauthorized),
+    _402(Name.PaymentRequired);
+    final Name name;
+
+    Number(Name name) {
+        this.name = name;
+    }
+
+}
