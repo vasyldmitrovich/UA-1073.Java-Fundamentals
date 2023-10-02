8fc291792961fd611dd1bb45752fb0585285dc94
Zamorii <sezamua@gmail.com>
2023-10-02 14:32:25 +0300

    Practical tasks 1 in edu05 done

---
+++ src/com/softserve/edu05/pt/App.java
@@ -0,0 +1,7 @@
+package com.softserve.edu05.pt;
+
+public class App {
+    public static void main(String[] args) {
+        Task1.run();
+    }
+}
---
+++ src/com/softserve/edu05/pt/Task1.java
@@ -0,0 +1,30 @@
+package com.softserve.edu05.pt;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void run() {
+        System.out.println("Task 1 running ...");
+        String[] names = {"Lilly", "John", "Bob", "Anna", "Sarah", "Denis"};
+        System.out.println("Unsorted String array: " + Arrays.toString(names));
+        Arrays.sort(names);
+        System.out.println("Sorted String array: " + Arrays.toString(names));
+        int[] numbers = {5, 12, 34, 5, 8, 99, 11, 5, 45, 67};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("Integer array: " + Arrays.toString(numbers));
+        System.out.println("Average value for integer array is " + (double) sum / numbers.length);
+        System.out.print("Enter name to search: ");
+        Scanner console = new Scanner(System.in);
+        String enteredName = console.nextLine();
+        boolean isName = false;
+        for (String name : names) {
+            isName = name.equals(enteredName) ? true : isName;
+        }
+        System.out.println("The entered name is " + (isName ? "present" : "not present") + " in the String array");
+    }
+}
