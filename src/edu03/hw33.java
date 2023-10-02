c232028e5d06e051bc317421f1a2e313af7829a0
l1ubek <alluluciano03@gmail.com>
2023-10-02 18:03:59 +0300

    dz

---
+++ src/edu03/hw33.java
@@ -0,0 +1,26 @@
+package edu03;
+
+import java.util.Scanner;
+public class hw33 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" Вкажіть три числа ");
+        int num = scanner.nextInt();
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int smallest = smallestt(num, num1, num2);
+        System.out.println(" Найменше число - " + smallest);
+    }
+    public static int smallestt(int num, int num1, int num2) {
+        int smallest = num;
+        if (num1 < smallest) {
+            smallest = num1;
+        }
+        if (num2 < smallest) {
+            smallest = num2;
+        }
+        return smallest;
+    }
+}
+
