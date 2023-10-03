d66444ef57533d44f455932de5c6fb2fa7c8f680
Zamorii <sezamua@gmail.com>
2023-10-03 21:52:35 +0300

    Practical tasks 1 - 4, homework task 1 in edu05 done

---
+++ src/com/softserve/edu05/hw/App.java
@@ -0,0 +1,13 @@
+package com.softserve.edu05.hw;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Task 1 running ...");
+        System.out.println("Subtask 1.1 running ...");
+        Task1_1.run();
+        System.out.println("Subtask 1.2 running ...");
+        Task1_2.run();
+        System.out.println("Subtask 1.3 running ...");
+        Task1_3.run();
+    }
+}
