c7a643c0cb82394c420bdcb655ae285fd111a020
Zamorii <sezamua@gmail.com>
2023-10-04 01:20:29 +0300

    Practical tasks 1 - 4, homework tasks 1 - 4 in edu05 done Methods review

--- src/com/softserve/edu05/hw/Task1_1.java
+++ src/com/softserve/edu05/hw/Task1_1.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class Task1_1 {
     public static void run() {
-        int[] monthDaysCount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         Scanner console = new Scanner(System.in);
         System.out.print("Enter month number: ");
         int monthNumber = console.nextInt();
         console.nextLine();
-        System.out.println("In this month " + monthDaysCount[monthNumber - 1] + " days");
+        System.out.println("In this month " + getMonthDays(monthNumber) + " days");
+    }
+
+    static int getMonthDays(int monthNumber) {
+        int[] monthDaysCount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return monthDaysCount[monthNumber - 1];
     }
 }
