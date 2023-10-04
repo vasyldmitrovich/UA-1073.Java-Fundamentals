d2860b44015ee839bb3c23b5f220472000e48a34
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2023-10-04 18:27:02 +0300

    Add jUnit tests

--- tests/com/softserve/edu05/apptest/CalcTest.java
+++ tests/com/softserve/edu05/apptest/CalcTest.java
@@ -1,64 +1,39 @@
 package com.softserve.edu05.apptest;
 
-/*<<<<<<< HEAD
 
-import org.junit.jupiter.api.*;
+/*import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-=======
-import org.junit.*;
->>>>>>> b5dcf80 (Add jUnit tests)
 
 public class CalcTest {
     private static Calc calc;
 
-<<<<<<< HEAD
     @BeforeAll
-=======
-    @BeforeClass
->>>>>>> b5dcf80 (Add jUnit tests)
     public static void setUpBeforeClass() throws Exception {
         System.out.println("@BeforeClass setUpBeforeClass()");
         calc = new Calc();
     }
 
-<<<<<<< HEAD
     @AfterAll
-=======
-    @AfterClass
->>>>>>> b5dcf80 (Add jUnit tests)
     public static void tearDownAfterClass() throws Exception {
         System.out.println("@AfterClass tearDownAfterClass()");
     }
 
-<<<<<<< HEAD
     @BeforeEach
-=======
-    @Before
->>>>>>> b5dcf80 (Add jUnit tests)
     public void setUp() throws Exception {
         System.out.println("\t@Before setUp()");
     }
 
-<<<<<<< HEAD
     @BeforeEach
-=======
-    @Before
->>>>>>> b5dcf80 (Add jUnit tests)
     public void setUp2() throws Exception {
         System.out.println("\t@Before setUp2()");
     }
 
-<<<<<<< HEAD
     @AfterEach
-=======
-    @After
->>>>>>> b5dcf80 (Add jUnit tests)
     public void tearDown() throws Exception {
         System.out.println("\t@After tearDown()");
     }
 
-<<<<<<< HEAD
     @Test
     public void testException() {
         Assertions.assertThrows(ArithmeticException.class, ()->{
@@ -68,14 +43,6 @@ public class CalcTest {
             System.out.println("\t\t\ti = " + i);
         });
 
-=======
-    @Test(expected = ArithmeticException.class)
-    public void testException() {
-        System.out.println("\t\t@Test testException()");
-        int i = 0;
-        i = 10 / i;
-        System.out.println("\t\t\ti = " + i);
->>>>>>> b5dcf80 (Add jUnit tests)
     }
 
     @Test
@@ -98,11 +65,7 @@ public class CalcTest {
         actual = calc.add(4, 5);
         expected = 9.0001;
 
-<<<<<<< HEAD
         assertEquals(expected, actual, 0.001);
-=======
-        Assert.assertEquals(expected, actual, 0.001);
->>>>>>> b5dcf80 (Add jUnit tests)
         System.out.println("\t\t@Test testAdd1() - work correct");
     }
 
@@ -117,11 +80,7 @@ public class CalcTest {
         actual = calc.add(4, 6);
         expected = 10;
         //
-<<<<<<< HEAD
         assertEquals(expected, actual, 0.001);
-=======
-        Assert.assertEquals(expected, actual, 0.001);
->>>>>>> b5dcf80 (Add jUnit tests)
     }
 
     @Test
@@ -135,11 +94,7 @@ public class CalcTest {
         actual = calc.div(20, 4);
         expected = 5;
         //
-<<<<<<< HEAD
         assertEquals(expected, actual, 0.001);
-=======
-        Assert.assertEquals(expected, actual, 0.001);
->>>>>>> b5dcf80 (Add jUnit tests)
     }
 
     @Test
@@ -153,11 +108,7 @@ public class CalcTest {
         actual = calc.div(20, 8);
         expected = 2.5;
         //
-<<<<<<< HEAD
         assertEquals(expected, actual, 0.001);
-=======
-        Assert.assertEquals(expected, actual, 0.001);
->>>>>>> b5dcf80 (Add jUnit tests)
     }
 
 }
