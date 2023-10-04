7bd567a71f6d6085748bda2589a477934e1b8e9e
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2023-10-04 18:27:02 +0300

    Add jUnit tests

--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,20 +1,17 @@
 package com.softserve.edu01.pt;
 
-import java.io.IOException;
-
 public class App {
     public static void main(String[] args) {
- //Practical task
-        //Task 1
-        Task_1.task1();
 
-        //Task 2
-        Task_2.task2();
+        // Practical tasks
 
-        //Task 3
-        Task_3.task3();
+        // Task 1
+        //Task1.task1();
 
-        //Task 4
-        Task_4.task4();
+        // Task 2
+        //Task2.task2();
+
+        // Task 3...
+
     }
 }
