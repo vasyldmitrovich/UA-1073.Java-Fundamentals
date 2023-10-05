af546e05b5ddcd39b09f5b2b9db3d8b834d7c124
switcher-ua <vovkotrubpp@gmail.com>
2023-10-05 18:25:33 +0300

    add unitTests

---
+++ src/com/softserve/edu06/hw/App.java
@@ -0,0 +1,8 @@
+package com.softserve.edu06.hw;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/softserve/edu06/pt/App.java
@@ -0,0 +1,8 @@
+package com.softserve.edu06.pt;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
