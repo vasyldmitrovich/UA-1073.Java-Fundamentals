d9cf060d43553f885eaaf5bde13b899fce75ae09
Zamorii <sezamua@gmail.com>
2023-10-08 11:03:03 +0300

    Practical tasks 1 - 4, homework tasks 1 - 4 in edu05 done. Task1_1 test added.

---
+++ tests/com/softserve/edu05/hw/Task1_1Test.java
@@ -0,0 +1,16 @@
+package com.softserve.edu05.hw;
+
+import org.junit.jupiter.api.Test;
+
+import static com.softserve.edu05.hw.Task1_1.getMonthDays;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task1_1Test {
+
+    @Test
+    void testGetMonthDays() {
+        assertEquals(28, getMonthDays(2), "February");
+        assertEquals(31, getMonthDays(12), "December");
+        assertEquals(30, getMonthDays(6), "June");
+    }
+}
