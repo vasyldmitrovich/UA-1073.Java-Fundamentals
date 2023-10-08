98f331086531c7aca5c6bbcefe40113b41ce1d12
Zamorii <sezamua@gmail.com>
2023-10-08 11:59:54 +0300

    Practical tasks 1 - 4, homework tasks 1 - 5 in edu05 done.

--- tests/com/softserve/edu05/hw/Task1_3Test.java
+++ tests/com/softserve/edu05/hw/Task1_3Test.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class Task1_3Test {
     private int[] array1 = {-4, 5, 0, 2, 8};
     private int[] array2 = {-3, -8, 0, 5, 6};
-    private int[] array3 = {-5, 4, 0, 2, -8};
+    private int[] array3 = {-6, 4, 0, 2, -8};
     @Test
     void testSecondPositiveIndex() {
         assertEquals(3, secondPositiveIndex(array1));
@@ -27,6 +27,6 @@ class Task1_3Test {
     void testEvenProduct() {
         assertEquals(-64, evenProduct(array1));
         assertEquals(-48, evenProduct(array2));
-        assertEquals(-64, evenProduct(array3));
+        assertEquals(384, evenProduct(array3));
     }
 }
