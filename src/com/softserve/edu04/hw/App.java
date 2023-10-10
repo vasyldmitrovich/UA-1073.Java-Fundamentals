4b95f690b753c5af361ea1f621ab3ab47c156106
arsentiev_artur <arturarsentiev25@gmail.com>
2023-10-10 13:47:06 +0300

    правки task4.

--- src/com/softserve/edu04/hw/App.java
+++ src/com/softserve/edu04/hw/App.java
@@ -5,15 +5,17 @@ import com.softserve.edu04.hw.task1.Task1Edu04Hw;
 import com.softserve.edu04.hw.task2.Task2Edu04Hw;
 import com.softserve.edu04.hw.task3.Task3Edu04Hw;
 import com.softserve.edu04.hw.task4.Task4Edu04Hw;
+import com.softserve.edu04.hw.task5.Task5Edu04Hw;
 
 public class App {
 
     public static void main(String[] args) {
 
-        Task1Edu04Hw.task1(); //Task1
-        Task2Edu04Hw.task2(); //Task2
-        Task3Edu04Hw.task3Edu04Hw(); // Task3
-        Task4Edu04Hw.task4(); //Task4
+        Task1Edu04Hw.task1();           //Task1
+        Task2Edu04Hw.task2();           //Task2
+        Task3Edu04Hw.task3Edu04Hw();    //Task3
+        Task4Edu04Hw.task4();           //Task4
+        Task5Edu04Hw.task5Edu04Hw();    //Task5
 
 
 
