652c9ea70b748c345f987d80d91b6dacdcb9badc
Maksym Pyvovarov <119981087+maxpv026@users.noreply.github.com>
2023-10-12 16:57:46 +0300

    hw03

---
+++ src/com/softserve/edu03/hw/Task1.java
@@ -0,0 +1,26 @@
+/*Write Java console application with method to calculate the area of a triangle.*/
+
+package com.softserve.edu03.hw;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static double area;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input 3 values to calculate the area");
+        double s1 = scan.nextDouble();
+        double s2 = scan.nextDouble();
+        double s3 = scan.nextDouble();
+        area = calculateArea(s1, s2, s3);
+        System.out.format("\nThe Area of the triangle = %.2f\n", area);
+        scan.close(); // Close the Scanner to prevent resource leak
+    }
+
+    public static double calculateArea(double s1, double s2, double s3) {
+        double s = (s1 + s2 + s3) / 2;
+        area = Math.sqrt(s * (s - s1) * (s - s2) * (s - s3));
+        return area;
+    }
+}
