fb35fc3bd83f339c5833c44b46050eed31ed4d35
Zamorii <sezamua@gmail.com>
2023-10-15 22:08:39 +0300

    Practical task, homework in edu08 done.

--- src/com/softserve/edu08/hw/App.java
+++ src/com/softserve/edu08/hw/App.java
@@ -2,14 +2,15 @@ package com.softserve.edu08.hw;
 
 public class App {
     public static void main(String[] args) throws CloneNotSupportedException {
+        System.out.println("Creating two students ...");
         Student[] students = new Student[3];
         students[0] = new Student(new FullName("Bob", "Marley"), 20, 3);
         students[1] = new Student("Alice", "Cooper", 19, 2);
         printAllStudents(students, 2);
-        System.out.println("Cloning the first student ...");
+        System.out.println("\nCloning the first student ...");
         students[2] = (Student) students[0].clone();
         printAllStudents(students, 3);
-        System.out.println("Change course number for last student ...");
+        System.out.println("\nChange course number for the third student ...");
         students[2].setCourse(5);
         printAllStudents(students, 3);
     }
