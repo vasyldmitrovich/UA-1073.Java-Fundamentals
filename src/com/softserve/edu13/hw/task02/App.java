74f942ce62dfb0a7c164b64f7fbd8d2038d796cc
Zamorii <sezamua@gmail.com>
2023-10-31 11:05:31 +0200

    Practical task, homework 1 - 2 in edu13 done

---
+++ src/com/softserve/edu13/hw/task02/App.java
@@ -0,0 +1,28 @@
+package com.softserve.edu13.hw.task02;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class App {
+    static final Scanner SCANNER = new Scanner(System.in);
+    public static void main(String[] args) {
+        String enDate = "";
+        do {
+            System.out.print("Enter date in format mm.dd.yy: ");
+            enDate = SCANNER.nextLine();
+        } while (!isValidDate(enDate, "MM.dd.yy"));
+    }
+
+    static boolean isValidDate(String enDate, String pattern) {
+        try {
+            LocalDate.parse(enDate, DateTimeFormatter.ofPattern(pattern));
+            System.out.println("Entered date is valid!");
+            return true;
+        } catch (DateTimeParseException e) {
+            System.out.println("Entered date is invalid!");
+            return false;
+        }
+    }
+}
