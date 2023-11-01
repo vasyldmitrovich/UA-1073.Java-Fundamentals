ad28fa86ab4990a996aa6ebe3c8ee0cc6b031709
arsentiev_artur <arturarsentiev25@gmail.com>
2023-11-01 21:44:24 +0200

    add task 1 and 2. edu08 Hw.

--- src/com/softserve/edu08/hw/task1/Task_1_and_2_Edu08Hw.java
+++ src/com/softserve/edu08/hw/task1/Task_1_and_2_Edu08Hw.java
@@ -5,7 +5,7 @@ public class Task_1_and_2_Edu08Hw {
     public static void main(String[] args) {
 
         Student student1 = new Student(new FullName("Artur", "Arsen"), 30, 4);
-        Student student2 = new Student(new FullName("Ros", "Geler"), 35, 3);
+        Student student2 = new Student(new FullName("Ros", "Galler"), 35, 3);
         Student student3 = new Student(new FullName("Boni", "Pug"), 22, 2);
 
         System.out.println(student1.activity());
